8546cf728af9134362c64fa7e02a1731bb4eb224
Pat Monette <patrick@ontarioseo.ca>
2020-02-06 20:20:11 +0000

    add media coverage

--- wp-content/themes/annamaes/index.php
+++ wp-content/themes/annamaes/index.php
@@ -18,8 +18,10 @@ get_header(); ?>
 <div class="main-container">
 	<div class="main-grid">
 		<main class="main-content">
+		<?php if(is_home()): ?>
+			<h1 class="hblock">Media <strong>Coverage</strong></h1>
+		<?php endif; ?>
 		<?php if ( have_posts() ) : ?>
-
 			<?php /* Start the Loop */ ?>
 			<?php while ( have_posts() ) : the_post(); ?>
 				<?php get_template_part( 'template-parts/content', get_post_format() ); ?>
